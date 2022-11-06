423d112fda05814630a8d5c71209857b2293cc3c
narasimman97 <narasimma97gmail.com>
2022-11-07 03:39:55 +0530

    Added three methods from Practice

--- src/test/java/com/sample/Sample.java
+++ src/test/java/com/sample/Sample.java
@@ -14,5 +14,17 @@ public class Sample {
 		System.out.println("japan");
 
 	}
+	private void method1() {
+		System.out.println("method1");
+
+	}
+	private void method2() {
+		System.out.println("method2");
+
+	}
+	private void method3() {
+		System.out.println("method 3");
+
+	}
 	
 }
