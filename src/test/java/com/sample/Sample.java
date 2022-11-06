af8ce36f0b60464abad26d5705638911c0379f55
narasimman97 <narasimma97gmail.com>
2022-11-06 21:55:35 +0530

    first time push

---
+++ src/test/java/com/sample/Sample.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class Sample {
+
+	private void india() {
+		System.out.println("india");
+		
+	}
+	private void australia() {
+		System.out.println("australia");
+
+	}
+	private void japan() {
+		System.out.println("japan");
+
+	}
+	
+}
